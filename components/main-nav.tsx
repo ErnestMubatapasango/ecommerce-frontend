e07b6cb6d741e15216636ab4fa0443e64f06d67f
ErnestMubatapasango <tawazzmubazz@gmail.com>
2024-02-21 11:59:02 +0200

    added the type interfaces for the main nav data prop

---
+++ components/main-nav.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+interface MainNavProps {
+    data: Category[]
+}
+const MainNav: React.FC<MainNavProps> = ({data}) => {
+
+    const pathname = usePathname();
+    const routes = data.map((route) => {
+        return {
+            href: `/category/${route.id}`,
+            label: route.name,
+            active: pathname === `/category/${route.id}`
+        }
+    })
+  return (
+    <nav className="mx-6 flex items-center gap-4">
+        {routes.map((route) => (
+            <Link
+                href={route.href}
+                key={route.href}
+                className={cn("text-sm font-medium transition-colors hover:text-black",
+                    route.active ? "text-black": "text-neutral-500"
+                )}
+            >
+                {route.label}
+            </Link>
+        ))}
+    </nav>
+  )
+}
+
+export default MainNav
