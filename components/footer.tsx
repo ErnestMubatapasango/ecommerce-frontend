0fe4660e47b4e442c9a9de27c7bcf1320c51a946
ErnestMubatapasango <tawazzmubazz@gmail.com>
2024-02-21 11:52:52 +0200

    created the footer and container comps

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='border-t'>
+        <p className="text-black text-center py-10">&copy; 2024 Digital Bots Store, Inc. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
