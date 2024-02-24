048cf3fa31fe5b45ca80ff587a987f0172eb86af
ErnestMubatapasango <tawazzmubazz@gmail.com>
2024-02-24 14:33:34 +0200

    add: api call to categories API

---
+++ actions/get-categories.tsx
@@ -0,0 +1,10 @@
+
+const URL = `${process.env.NEXT_PUBLIC_URL_API}/categories`
+
+const getCategories = async (): Promise<Category[]> => {
+    const res = await fetch(URL)
+    
+    return res.json()
+}
+
+export default getCategories
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Container from '@/components/ui/container'
 import Link from 'next/link'
 import MainNav from './main-nav'
+import getCategories from '@/actions/get-categories'
 
-const Navbar = () => {
+export const revalidate = 0;
+
+const Navbar = async() => {
+
+  const categories = await getCategories()
+  
   return (
     <div className='border-b'>
         <Container>
@@ -11,7 +17,7 @@ const Navbar = () => {
                 <Link href='/' className='ml-4 lg:ml-0 gap-x-2'>
                     <p className='font-bold text-xl'>STORE</p>
                 </Link>
-                <MainNav data={[]} />
+                <MainNav data={categories} />
             </div>
         </Container>
     </div>
