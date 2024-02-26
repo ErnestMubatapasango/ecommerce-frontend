f8e4c5d35bdad98f744d77fcae8c3ab037c21853
ErnestMubatapasango <tawazzmubazz@gmail.com>
2024-02-26 19:52:51 +0200

    created  a no results comp and updated the billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,17 +2,24 @@ import Billboard from '@/components/billboard'
 import getBillboard from '@/actions/get-billboard'
 import React from 'react'
 import Container from '@/components/ui/container'
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
 
 
 export const revalidate = 0;
 
 const HomePage = async() => {
 
-  const billboard = await getBillboard("65db690ffd0a46bf1b20de0a")
+  const billboard = await getBillboard("65db690ffd0a46bf1b20de0a");
+  const products = await getProducts({isFeatured: true});
+
   return (
     <Container>
       <div className='space-y-10 pb-10'> 
         <Billboard  data={billboard}/>
+      </div>
+      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+        <ProductList title="Featured Products" items={products}/>
       </div>
     </Container>
   
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoResults = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center font-medium text-2xl'>
+        No results found
+    </div>
+  )
+}
+
+export default NoResults
